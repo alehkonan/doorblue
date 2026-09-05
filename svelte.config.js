bd8d02a5bb4287c36cf8a763076b7a2bdc6a1924
Aleh <olegkonanff@gmail.com>
2026-09-05 11:21:56 +0400

    style: format project configuration and critique

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,14 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from "@sveltejs/adapter-cloudflare";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter({
       platformProxy: {
-        configPath: 'wrangler.toml'
-      }
-    })
-  }
+        configPath: "wrangler.toml",
+      },
+    }),
+  },
 };
 
 export default config;
